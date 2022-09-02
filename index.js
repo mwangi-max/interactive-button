35d254e420eccb6bdbc140a2f19383b33bf9b99e
Kelvin Kamau <80668152+mwangi-max@users.noreply.github.com>
2022-09-02 13:53:22 +0300

    Interactive button
    JavaScript is Dynamic

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button');
+
+button.addEventListener('mouseover', (e)=>{
+    const x = (e.pageX - button.offsetLeft);
+    const y = (e.pageY - button.offsetTop);
+
+    button.style.setProperty('xPos', x +  'px');
+    button.style.setProperty('yPos', y +  'px');
+})
